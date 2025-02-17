ca1c1971bb6b8e147dbd5ea1c28d31b5f30cedf8
chunweishi18 <154118927+ChunweiShi@users.noreply.github.com>
2025-02-16 22:38:03 -0800

    Now supports Meteora dlmm pool interaction

--- meteora-swap-ts/swap.ts
+++ meteora-swap-ts/swap.ts
@@ -1,52 +1,89 @@
-import DLMM from '@meteora-ag/dlmm'
-import BN from "bn.js"
-
 import {
-    Connection,
-    PublicKey
-} from '@solana/web3.js'
+  Connection,
+  Keypair,
+  PublicKey,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
+import DLMM from "@meteora-ag/dlmm";
+import BN from "bn.js";
+import { BinLiquidity, LbPosition, StrategyType } from "@meteora-ag/dlmm";
+import dotenv from 'dotenv'
 
-const BASE_MINT = 'So11111111111111111111111111111111111111112'
-const QUOTE_MINT = 'DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263'
-const amount = '0.00001'
+dotenv.config()
 
-const USDC_USDT_POOL = new PublicKey('ARwi1S4DaiTG5DX7S4M4ZsrXqpMD1MrTmbu9ue2tpmEq') // You can get your desired pool address from the API https://dlmm-api.meteora.ag/pair/all
+if (!process.env.USER_PRIVATE_KEY) {
+  throw new Error('Undefined User Private Key')
+}
 
-async function swapQuote(
-    poolAddress: PublicKey,
-    swapAmount: BN,
-    swapYtoX: boolean,
-    isPartialFill: boolean
-  ) {
-    let rpc = "https://api.mainnet-beta.solana.com";
-    const connection = new Connection(rpc, "finalized");
-    const dlmmPool = await DLMM.create(connection, poolAddress, {
-      cluster: "mainnet-beta",
-    });
-  
-    const binArrays = await dlmmPool.getBinArrayForSwap(swapYtoX);
-    const swapQuote = await dlmmPool.swapQuote(
-      swapAmount,
-      swapYtoX,
-      new BN(10),
-      binArrays,
-      isPartialFill
-    );
-    console.log("🚀 ~ swapQuote:", swapQuote);
-    console.log(
-      "consumedInAmount: %s, outAmount: %s",
-      swapQuote.consumedInAmount.toString(),
-      swapQuote.outAmount.toString()
-    );
+const user = Keypair.fromSecretKey(
+  new Uint8Array(bs58.decode(process.env.USER_PRIVATE_KEY))
+);
+const RPC = process.env.RPC || "https://api.mainnet-beta.solana.com";
+const connection = new Connection(RPC, "finalized");
+
+const poolAddress = new PublicKey(
+  "5rCf1DM8LjKTw4YqhnoLcngyZYeNnQqztScTogYHAS6"
+);
+
+/** Utils */
+export interface ParsedClockState {
+  info: {
+    epoch: number;
+    epochStartTimestamp: number;
+    leaderScheduleEpoch: number;
+    slot: number;
+    unixTimestamp: number;
+  };
+  type: string;
+  program: string;
+  space: number;
+}
+
+let activeBin: BinLiquidity;
+let userPositions: LbPosition[] = [];
+
+const newBalancePosition = new Keypair();
+const newImbalancePosition = new Keypair();
+const newOneSidePosition = new Keypair();
+
+async function swap(dlmmPool: DLMM) {
+  const swapAmount = new BN(1000000);
+  // Swap quote
+  const swapYtoX = true;
+  const binArrays = await dlmmPool.getBinArrayForSwap(swapYtoX);
+
+  const swapQuote = await dlmmPool.swapQuote(swapAmount, swapYtoX, new BN(10), binArrays);
+
+  console.log("🚀 ~ swapQuote:", swapQuote);
+
+  // Swap
+  const swapTx = await dlmmPool.swap({
+    inToken: dlmmPool.tokenX.publicKey,
+    binArraysPubkey: swapQuote.binArraysPubkey,
+    inAmount: swapAmount,
+    lbPair: dlmmPool.pubkey,
+    user: user.publicKey,
+    minOutAmount: swapQuote.minOutAmount,
+    outToken: dlmmPool.tokenY.publicKey,
+  });
+
+  try {
+    const swapTxHash = await sendAndConfirmTransaction(connection, swapTx, [
+      user,
+    ]);
+    console.log("🚀 ~ swapTxHash:", swapTxHash);
+  } catch (error) {
+    console.log("🚀 ~ error:", JSON.parse(JSON.stringify(error)));
   }
-  
+}
+
 async function main() {
-    await swapQuote(
-      new PublicKey("8kCbYxnF8ggdJACxz4NLYtVhEEz6EBvN5NQcKAazpkEY"),
-      new BN(1_000_000_000),
-      true,
-      true
-    );
-  }
-  
+  const dlmmPool = await DLMM.create(connection, poolAddress, {
+    cluster: "mainnet-beta",
+  });
+
+  await swap(dlmmPool);
+}
+
 main();
